59baaadae22ead534916a8b0b862fcf322110efa
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-03 18:58:06 +0100

    set up everythig

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useGlobalContext } from '../context';
 
 const Navbar = () => {
+  const value = useGlobalContext();
+  console.log(value);
+
   return (
     <nav>
       <div className="nav-center">
         <Link to="/">the planets</Link>
+        <GiHamburgerMenu />
       </div>
     </nav>
   );
