5e630224bf3ab0244c08e8f686dfaeaa4852d159
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-03 19:10:33 +0100

    some changes"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
+// pages
 import Home from './pages/Home';
 import SinglePlanet from './pages/SinglePlanet';
 import Error from './pages/Error';
+
+// components
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Sidebar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/plante/:id" element={<SinglePlanet />} />
---
+++ src/components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from '../assets/planet-earth.svg';
+console.log(logo);
+
+const Sidebar = () => {
+  return <aside>sidebar</aside>;
+};
+
+export default Sidebar;
